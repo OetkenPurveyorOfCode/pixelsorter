f759fb3be972c06586b0a017c0f3c645b4a36b42
OetkenPurveyorOfCode <oetkenpurveyorofcode@gmail.com>
2024-11-25 22:05:04 +0100

    add build for linux

--- build_release.bat
+++ build_release.bat
@@ -1,4 +1,4 @@
 mkdir build
 pushd build
-call clang -Wall -Wextra -std=c11 -O3 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_windows.c -luser32 -lgdi32 -lwinmm -o main.exe
+call clang -Wall -Wextra -std=c11 -O3 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_windows.c -luser32 -lgdi32 -lwinmm -o pixelsorter_win.exe
 popd build
