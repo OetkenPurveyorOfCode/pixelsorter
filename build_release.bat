d5dabcd0e7e7c84f670d29d8085e04b2f6d2f906
OetkenPurveyorOfCode <oetkenpurveyorofcode@gmail.com>
2024-11-25 21:58:07 +0100

    add mkdir in build

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,4 @@
+mkdir build
 pushd build
 call clang -Wall -Wextra -std=c11 -O3 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_windows.c -luser32 -lgdi32 -lwinmm -o main.exe
 popd build
