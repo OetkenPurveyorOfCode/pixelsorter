2f67f1e47e537be9396bc677e0abef1b6f22f377
OetkenPurveyorOfCode <oetkenpurveyorofcode@gmail.com>
2024-11-25 22:01:31 +0100

    add mkdir

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-
+mkdir build
 pushd build
 call clang -g -Wall -Wextra -fsanitize=address,undefined -std=c11 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_windows.c -luser32 -lgdi32 -lwinmm -o main.exe
 popd build
--- build_release.sh
+++ build_release.sh
@@ -1,3 +1,4 @@
+mkdir build
 cd build
 gcc -Wall -Wextra -std=c11 -O2 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_linux.c -lX11 -lm -o main_linux.exe
 cd ..
