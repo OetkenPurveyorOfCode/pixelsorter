1a071da290ac938d2854abd3a7ebafe9b76e8eca
OetkenPurveyorOfCode <oetkenpurveyorofcode@gmail.com>
2024-11-25 22:16:54 +0100

    delete file to add it again Please enter the commit message for your changes. Lines starting

--- build_release.sh
+++
@@ -1,4 +0,0 @@
-mkdir build
-cd build
-gcc -Wall -Wextra -std=c11 -O2 -I../3rdparty/minifb/include/ -I../3rdparty/minifb/src/ ../main.c ../3rdparty/ffmpeg/ffmpeg_linux.c -lX11 -lm -o pixelsorter_linux
-cd ..
